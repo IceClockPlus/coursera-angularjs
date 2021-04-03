c0911c3f2fdd099c545e6b30c603c0b1dbe39172
IceClockPlus <aravenapro98@gmail.com>
2021-04-03 19:24:44 -0300

    Implementacion del primer componente Angular

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+//IIFE (Funcion invocada inmediatamente)
 (function(){
 'user strict';
 
 angular.module('myFirstApp', [])
-.controller('myFirstController', function(){
-        
+.controller('myFirstController', function($scope){
+    $scope.name = "Alexis"    
+    $scope.sayHello = function() {
+        return "Hello Coursera!"
+    };
 });
 
 } )();
